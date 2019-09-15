0376495ca9040619eaa6ab56e8c9d2d6bd2e5bc9
jadarya <jadarya@mail.ru>
2019-09-16 08:32:42 +0900

    Added one

--- Models/Telegram/InlineKeyboardMarkup.cs
+++ Models/Telegram/InlineKeyboardMarkup.cs
@@ -14,5 +14,15 @@ namespace MyTelegramBot.Models.Telegram
         public List<List<InlineKeyboardButton>> inline_keyboard { get; set; }
         public void AddButton(InlineKeyboardButton button) =>
             this.inline_keyboard.Add(new List<InlineKeyboardButton> { button });
+        public void AddLineButtons(List<InlineKeyboardButton> lineButtons) =>
+            this.inline_keyboard.Add(lineButtons);
+        public void AddButton(InlineKeyboardButton button, int numLine)
+        {
+            while(inline_keyboard.Count <= numLine)
+            {
+                inline_keyboard.Add(new List<InlineKeyboardButton>());
+            }
+            inline_keyboard[numLine].Add(button);
+        }
     }
 }
